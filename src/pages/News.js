4319ca811dbdc5fd0599a2b96913d675e21b7429
Hamlet Kirakosyan <hamletkirakosyan1991@gmail.com>
2022-04-01 22:35:43 +0400

    added <News /> component with any css

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import "./news.css"
 
 export default function News() {
   return (
-    <div>News</div>
+    <div className='newsContainer'>
+        <div className="newsName">News</div>
+        <div className="newsInfo">
+            <div className="newsImage">NewsImage</div>
+            <div className="newsContent">
+                <span>NewsContent</span>
+                <button className="more">More</button>
+            </div>
+        </div>
+        <div className="newsLikeComments">
+            <div className="newsLike">Like</div>
+            <div className="newsComments">Comments</div>
+        </div>
+    </div>
   )
 }
